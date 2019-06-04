84482845ede5ae2db4ba2a3bbd169fe7be9616b9
fordemx <mforde84@gmail.com>
2019-06-04 14:05:40 -0500

    changed optional scope of git add 2019-06-04

--- commit.sh
+++ commit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Add what precisely (default --all): "
+read WHAT
+if [[ -z "$WHAT" ]]; then export WHAT="--all"; fi
+
 echo "Please enter a comment for commit: "
 read COMMENT
 
@@ -19,7 +23,7 @@ git config --global core.excludesFile .gitignore
 git remote set-url origin https://github.com/mforde84/salt_vagrant.git 
 	# firewall isn't allowing git@github.com:mforde84/salt_vagrant.git
 	# switch to enable rsa key
-git add --all
+git add "$WHAT"
 git commit -m "$COMMENT $(date +%F)"
 git push origin "$BRANCH"
 kill -9 "$ID"
