95f4521d5727fb03ff988fcbb160f8a440dc3283
mforde84 <mforde84@gmail.com>
2019-06-04 14:25:11 -0500

    test all commit 2019-06-04

--- commit.sh
+++ commit.sh
@@ -16,8 +16,8 @@ fi
 #update git
 eval `ssh-agent -s` > "$PWD"/access/sagent
 export ID=`cat "$PWD"/access/sagent | sed 's/Agent pid //g'`
-ssh-add "$PWD"/access/slurm
-git config --global user.name "fordemx"
+ssh-add "$PWD"/access/salt.rsa
+git config --global user.name mforde84
 git config --global user.email mforde84@gmail.com
 git config --global core.excludesFile .gitignore
 git remote set-url origin https://github.com/mforde84/salt_vagrant.git 
