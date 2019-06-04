7f3ae21541ef9a39b28eff27636ac038c3943214
fordemx <mforde84@gmail.com>
2019-06-04 13:55:01 -0500

    spaces are evil

--- orka.sh
+++ orka.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # set dev root
-SALT_ROOT=$PWD
+SALT_ROOT="$PWD"
 
 # helper function to up / down vms
 updown() { ## updown < up / down > < hostname >
- cd $SALT_ROOT/$2
+ cd "$SALT_ROOT"/$2
  vagrant $1
 }
 
 # query update to geany paths?
 echo "update geany paths (y/n, default:n)?: "
 read DECISION
- 
+
 if [[ "$DECISION" == "y" ]];  then # if update
- 
+
  # replace paths
  sed -i 's~^base_path.*~base_path='"$SALT_ROOT"'~g' $SALT_ROOT/salt.geany
- NEWLOC=`echo $SALT_ROOT | sed 's/\//%2F/g'`
+ NEWLOC=`echo "$SALT_ROOT" | sed 's/\//%2F/g'`
  sed -i 's~%2F.*vagrant~'"$NEWLOC"'~g' $SALT_ROOT/salt.geany
  sed -i 's~^last_dir.*~last_dir='"$SALT_ROOT"'~g' $SALT_ROOT/salt.geany
-  
+
 fi # else do nothing
 
 # query up or down vms?
@@ -32,7 +32,7 @@ if [[ "$DECISION" == "d" ]];  then #if down
  # break down everything
  updown "destroy -f" master
  updown "destroy -f" minion1
- 
+
 else # if up (default)
 
  # spool up
