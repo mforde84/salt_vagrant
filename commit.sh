5692f9f163a492c4060e97a8df66af87ebaab808
fordemx <mforde84@gmail.com>
2019-06-04 13:16:26 -0500

    comment out passwdless rsa key access, firewall issue

--- commit.sh
+++ commit.sh
@@ -16,7 +16,9 @@ ssh-add $PWD/access/slurm
 git config --global user.name "fordemx"
 git config --global user.email mforde84@gmail.com
 git config --global core.excludesFile .gitignore
-git remote set-url origin git@github.com:mforde84/salt_vagrant.git
+git remote set-url origin https://github.com/mforde84/salt_vagrant.git 
+	# firewall isn't allowing git@github.com:mforde84/salt_vagrant.git
+	# switch to enable rsa key
 git add --all
 git commit -m "$COMMENT $(date +%F)"
 git push origin "$BRANCH"
