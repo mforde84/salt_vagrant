e34ed4e839b07b22b04086ff340b4bb53adcf972
fordemx <mforde84@gmail.com>
2019-06-04 13:41:43 -0500

    add query to update geany paths

--- orka.sh
+++ orka.sh
@@ -1,29 +1,51 @@
 #!/bin/bash
 
-export SALT_ROOT=$PWD
+# set dev root
+SALT_ROOT=$PWD
 
+# helper function to up / down vms
 updown() { ## updown < up / down > < hostname >
  cd $SALT_ROOT/$2
  vagrant $1
 }
 
+# interactive, up or down vms?
 echo "up or down (u/d, default:u)?: "
 read DECISION
 
-if [[ "$DECISION" == "d" ]];  then
+if [[ "$DECISION" == "d" ]];  then #if down
 
  # break down everything
  updown "destroy -f" master
  updown "destroy -f" minion1
 
-else
+ # query update to geany paths
+ echo "update geany paths (y/n, default:n)?: "
+ read DECISION
+ 
+ if [[ "$DECISION" == "y" ]];  then # if update
+ 
+  # replace paths
+  sed -i 's~^base_path.*~base_path='"$SALT_ROOT"'~g' $SALT_ROOT/salt.geany
+  NEWLOC=`echo $SALT_ROOT | sed 's/\//%2F/g'`
+  sed -i 's~%2F.*vagrant~'"$NEWLOC"'~g' $SALT_ROOT/salt.geany
+  sed -i 's~^last_dir.*~last_dir='"$SALT_ROOT"'~g' $SALT_ROOT/salt.geany
+  
+ fi # else do nothing
+ 
+else # if up (default)
 
+ # spool up
  updown up master
  updown up minion1
 
+ # set key state for minion
  cd "$SALT_ROOT"/master
  vagrant ssh -c 'yes | salt-key --accept=minion1' # ACCEPT ALL: salt-key -A
 
 fi
 
+# return to dev root
 cd "$SALT_ROOT"
+
+exit 0
